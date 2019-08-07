f8e7b2f5444db81e3ae1898105baed96e89b0402
xxaxxelxx <x@axxel.net>
2019-08-07 14:16:48 +0200

    Updated 2019-08-07 14:16

--- confupdater.sh
+++ confupdater.sh
@@ -21,29 +21,29 @@ test "x$TZ" == "x" && exit 1
 
 MYSQLCONTROL="mysql -u detector -p$DB_PASS -D silenceDB -P $DB_PORT -h $DB_HOST --skip-column-names"
 
-
-
-
 while true; do
     # STARTUP
     C_MNTPNTLIST="$(echo "SELECT mntpnt FROM status;" | mysql -u detector -p$DB_PASS -h $DB_HOST -P $DB_PORT -D silenceDB --skip-column-names)" #"
     for C_MNTPNT in $C_MNTPNTLIST; do
     C_MNTPNT_ID="$(echo "$C_MNTPNT" | sed 's|.*\:\/\/||' | sed 's|\.|\_|g' | sed 's|\/|\_|g')" #"
 
-    echo "extend $C_MNTPNT_ID ask_mysql.sh $C_MNTPNT $DB_HOST $DB_PORT $DB_PASS $ALIVE_LIMIT $TZ"
-
-#./ask_mysql.sh http://broadcast.ir-media-tec.com/bbradio-ch08.mp3 192.168.100.124 63306 rfc1830 10 Europe/Berlin
-
-#echo"extend disk_root /usr/local/homegrown/snmp/nagios/check_disk.sh /
-
-    
-
+    SNMPD_EXTEND_BLOCK=$"${SNMPD_EXTEND_BLOCK}|extend $C_MNTPNT_ID ask_mysql.sh $C_MNTPNT $DB_HOST $DB_PORT $DB_PASS $ALIVE_LIMIT $TZ"
 
     done 
+
+    test -r snmpd.conf && cp -f snmpd.conf /etc/snmp/snmpd.conf && \
+    echo "$SNMPD_EXTEND_BLOCK" | tr '\|' '\n' >> /etc/snmp/snmpd.conf
 
     exit
     sleep 1
 done
+
+
+
+
+
+
+
 
 exit $?
 
