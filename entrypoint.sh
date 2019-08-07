75306f6c7020079181bf25470757d1e1ae45e847
xxaxxelxx <x@axxel.net>
2019-08-07 11:37:30 +0200

    Updated 2019-08-07 11:37

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ echo "$1" | grep -i '^exit' > /dev/null && exit 0
 
 while true; do
 	test -r "/etc/snmp/snmpd.conf" && (
-	    sed -e "s|<SNMPD_HOST>|${ENV_SNMPD_HOST}|g" -i /etc/snmp/snmpd.conf
-	    sed -e "s|<SNMPD_COMMUNITY>|${ENV_SNMPD_COMMUNITY}|g" -i /etc/snmp/snmpd.conf 
+	    sed -e "s|<SNMPD_HOST>|${SNMPD_HOST}|g" -i /etc/snmp/snmpd.conf
+	    sed -e "s|<SNMPD_COMMUNITY>|${SNMPD_COMMUNITY}|g" -i /etc/snmp/snmpd.conf 
 	    )
 
 #    ./detectorrunner.sh $DETECTORHOST_IF $DETECTORHOST_IF_SPEED $DETECTORHOST_IF_MAXLOAD_PERCENT $DETECTORHOST_MAXCPULOAD_PERCENT $MYSQL_HOST $MYSQL_PORT $MYSQL_DETECTOR_PASSWORD $ALIVE_LIMIT
