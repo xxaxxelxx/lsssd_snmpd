341f81ab415d2d6f7e725238ea7d15782425be7f
xxaxxelxx <x@axxel.net>
2019-08-09 17:44:31 +0200

    Updated 2019-08-09 17:44

--- confupdater.sh
+++ confupdater.sh
@@ -30,7 +30,7 @@ while true; do
     C_MD5="$(echo "$C_MNTPNTLIST" | md5sum | awk '{print $1}')"
     date > C
 #    echo "x$C_MD5_PRE vs x$C_MD5" > MD5
-    sleep 10; continue
+    #sleep 10; continue
     test "x$C_MD5_PRE" == "x$C_MD5" && sleep 60 && continue
     date > D
     echo "x$C_MD5_PRE vs x$C_MD5" > MD5X
