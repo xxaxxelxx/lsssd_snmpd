4045d357d587bb5c4109c8012195ad1133ecc127
xxaxxelxx <x@axxel.net>
2019-08-12 09:56:22 +0200

    Updated 2019-08-12 09:56

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,12 @@ set -ex
 
 echo "$1" | grep -i '^exit' > /dev/null && exit 0
 
+SNMPD_HOST="$(ip a | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17 | head -n 1)" #"
+
 test -r "snmpd.conf" && (
-    #sed -e "s|<SNMPD_HOST>|${SNMPD_HOST}|g" -i snmpd.conf
+    sed -e "s|<SNMPD_HOST>|${SNMPD_HOST}|g" -i snmpd.conf
     sed -e "s|<SNMPD_COMMUNITY>|${SNMPD_COMMUNITY}|g" -i snmpd.conf 
     )
-
 
 while true; do
     ./confupdater.sh $MYSQL_HOST $MYSQL_PORT $MYSQL_DETECTOR_PASSWORD $ALIVE_LIMIT $TZ
