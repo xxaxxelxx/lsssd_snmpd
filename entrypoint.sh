19b18bad78cede37867dd93b5d5ca2b06313c584
xxaxxelxx <x@axxel.net>
2019-08-12 10:19:00 +0200

    Updated 2019-08-12 10:19

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -ex
 
 echo "$1" | grep -i '^exit' > /dev/null && exit 0
 
-SNMPD_HOST="$(ip a | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17 | head -n 1)" #"
+#SNMPD_HOST="$(ip a | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.17 | head -n 1)" #"
 
 test -r "snmpd.conf" && (
-    sed -e "s|<SNMPD_HOST>|${SNMPD_HOST}|g" -i snmpd.conf
+    #sed -e "s|<SNMPD_HOST>|${SNMPD_HOST}|g" -i snmpd.conf
     sed -e "s|<SNMPD_COMMUNITY>|${SNMPD_COMMUNITY}|g" -i snmpd.conf 
     )
 
